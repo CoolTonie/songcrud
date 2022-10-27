18f542268176926f0de4cc1db688f3793ccde2b4
CoolTonie <nwachukwua99@gmail.com>
2022-10-27 20:00:36 +0100

    create model, admin, add app to setting, create superuser

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+class song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released= models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    # ariste_id=
+
+class lyric(models.Model):
+    content = models.CharField(max_length=1500)
+    # song_id=
+ 
